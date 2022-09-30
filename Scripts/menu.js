3dbef8bec10cacd318d750644285a0f72e95a8c2
Laura Molina <lauramolinay@gmail.com>
2022-09-30 01:08:01 -0500

    Menu decente

---
+++ Scripts/menu.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("menuLateral").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("menuLateral").style.width = "0";
+  }
